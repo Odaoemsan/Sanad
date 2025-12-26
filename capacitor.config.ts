28173316bb52717b6aba5a6b13e39df51337c015
عدي مغلاج <odae570211@gmail.com>
2025-12-26 13:46:49 +0000

    فشل البناء في GitHub مجدداً. هناك خطأ في العثور على capacitor-android:8.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,25 +5,8 @@ const config: CapacitorConfig = {
   appName: 'sanad',
   webDir: 'out',
   server: {
-    // Note: 'hostname' is for local development live-reload only.
-    // It's not used in the production APK.
-    hostname: 'localhost:9002', 
     androidScheme: 'https'
-  },
-  plugins: {
-    CapacitorHttp: {
-      enabled: true,
-    },
-    SplashScreen: {
-      launchShowDuration: 2000,
-    },
-  },
-  // Ensure the web content is bundled with the app for offline access
-  android: {
-    appendUserAgent: 'Capacitor-Web-App',
-    // Bundled web assets are more performant and work offline
-    useBundledWebRuntime: true, 
-  },
+  }
 };
 
 export default config;
